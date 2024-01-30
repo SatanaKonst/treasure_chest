2c3495cd9383f80e16c1ad213cd827af6ef40336
Konstantin <Satana.Konst@gmail.com>
2024-01-30 12:06:09 +0300

    Переделано хранение истории добавления элементов в инвентарь

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,6 @@ export default {
           action: null,
           refreshTime: 30000,
           count: 0,
-          addTime: null
         },
         aid: {
           icon: './assets/icons/aid.png',
@@ -59,7 +58,6 @@ export default {
           },
           refreshTime: 30000,
           count: 0,
-          addTime: null
         },
         health: {
           icon: './assets/icons/health.png',
@@ -74,21 +72,20 @@ export default {
             }, 10000)
           },
           refreshTime: 0,
-          count: 50,
-          addTime: null
+          count: 50
         },
         team: {
           icon: './assets/icons/team.png',
           actionEventStart: null,
           action: null,
           refreshTime: 0,
-          count: null,
-          addTime: null
+          count: 0
         }
       },
       question: null, // В глобальном объекте на странице запуска приложения
       scanResult: null,
       inventar: {},
+      inventarHistory: {},
       play: true,
       currentQuestion: null,
       team: null
@@ -106,7 +103,7 @@ export default {
       /* eslint-enable */
 
       //Добавим иконку команды в инвентарь
-      this.addInventarItem('team', true, this.team);
+      this.addInventarItem('team', false, this.team);
 
       //Добавим жизнь
       this.addInventarItem('health', false);
@@ -122,9 +119,7 @@ export default {
             this.team = Number(prepareData[1]);
             break;
           case 'inventar':
-            if (this.inventarSrc[prepareData[1]]) {
-              this.addInventarItem(prepareData[1])
-            }
+            this.addInventarItem(prepareData[1], true)
             break;
           case 'question':
             this.currentQuestion = this.question[prepareData[1]];
@@ -146,28 +141,39 @@ export default {
     },
     addInventarItem(key, countInc = true, count = null) {
       let prepareKey = key.split('_');
+      let inventarKey = prepareKey[0];
 
       //Если элемента нет в инвентаре, то добавляем
-      if (!this.inventar[key]) {
-        this.inventar[key] = {...this.inventarSrc[prepareKey[0]]};
-        this.inventar[key].addTime = Date.now()
+      if (!this.inventar[inventarKey]) {
+        if (
+            this.inventarHistory[key] &&
+            this.inventarSrc[inventarKey].refreshTime &&
+            this.inventarHistory[key] + this.inventarSrc[inventarKey].refreshTime > Date.now()
+        ) {
+          return false;
+        }
+
+        this.inventar[inventarKey] = {...this.inventarSrc[inventarKey]};
+        this.inventarHistory[key] = Date.now()
+      } else {
+        //Если дата добавления + дата выдачи нового элемента больше текущей даты, то не добавляем
+        if (
+            this.inventarHistory[key] &&
+            this.inventar[inventarKey].refreshTime &&
+            this.inventarHistory[key] + this.inventar[inventarKey].refreshTime > Date.now()
+        ) {
+          return false;
+        }
       }
 
-      //Если дата добавления + дата выдачи нового элемента больше текущей даты, то добавляем
-      if (
-          this.inventar[key].addTime &&
-          this.inventar[key].refreshTime &&
-          this.inventar[key].addTime + this.inventar[key].refreshTime > Date.now()
-      ) {
-        if (countInc === true) {
-          this.inventar[key].count++;
-        }
-        this.inventar[key].addTime = Date.now()
+      //Инкремент кол-ва если разрешено
+      if (countInc === true) {
+        this.inventar[inventarKey].count++;
       }
 
       //Если установлена переписывание значения
       if (count !== null) {
-        this.inventar[key].count += Number(count);
+        this.inventar[inventarKey].count += Number(count);
       }
     }
   }
